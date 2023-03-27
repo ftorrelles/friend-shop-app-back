45d02ef8595f5e8c5a4d3ad9f8cb789c06e9098f
ftorrelles <torrellesf93@gmai.com>
2023-03-27 11:31:49 -0400

    change 27-03

--- src/controllers/purchase.controllers.js
+++ src/controllers/purchase.controllers.js
@@ -1,6 +1,7 @@
 const catchError = require("../utils/catchError");
 const Purchase = require("../models/Purchase");
 const Product = require("../models/Product");
+const Cart = require("../models/Cart");
 
 const getAll = catchError(async (req, res) => {
     const userId = req.user.id;
@@ -11,6 +12,18 @@ const getAll = catchError(async (req, res) => {
     return res.json(result);
 });
 
+const purchaseCart = catchError(async (req, res) => {
+    const cart = await Cart.findAll({
+        where: { userId: req.user.id },
+        attributes: ["quantity", "productId", "userId"],
+        raw: true,
+    });
+    await Purchase.bulkCreate(cart);
+    await Cart.destroy({ where: { userId: req.user.id } });
+    return res.json(cart);
+});
+
 module.exports = {
     getAll,
+    purchaseCart,
 };
--- src/routes/purchase.router.js
+++ src/routes/purchase.router.js
@@ -1,9 +1,9 @@
-const { getAll } = require("../controllers/purchase.controllers");
+const { getAll, purchaseCart } = require("../controllers/purchase.controllers");
 const express = require("express");
 const verifyJWT = require("../utils/verifyJWT");
 
 const purchaseRouter = express.Router();
 
-purchaseRouter.route("/").get(verifyJWT, getAll);
+purchaseRouter.route("/").get(verifyJWT, getAll).post(verifyJWT, purchaseCart);
 
 module.exports = purchaseRouter;
